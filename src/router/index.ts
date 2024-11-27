fe63f70e10c7f874b45c9b0dfccbf79074460f7c
shakib-anwar-nibir17 <nibirshakib@gmail.com>
2024-11-27 22:46:58 +0600

    router fixed

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,14 @@
 import { trpc } from "../config/trpc";
 import { campaignRouter } from "../module/Campaigns/campaign.route";
 import { categoryRouter } from "../module/Category/category.route";
+import { donationRouter } from "../module/Donation/donation.route";
 import { userRouter } from "../module/User/user.route";
 
 export const appRouter = trpc.router({
   user: userRouter,
   category: categoryRouter,
   campaign: campaignRouter,
+  donation: donationRouter,
 });
 
 export type AppRouter = typeof appRouter;
